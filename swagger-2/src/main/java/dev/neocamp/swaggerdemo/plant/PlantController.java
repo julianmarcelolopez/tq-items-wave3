ab1884a4327796045ea2ab657c44d5c970d7d6dd
Julian Marcelo Lopez <ext_lopjulia@mercadolibre.com>
2024-02-06 12:00:53 -0300

    adding swagger-2 example

---
+++ swagger-2/src/main/java/dev/neocamp/swaggerdemo/plant/PlantController.java
@@ -0,0 +1,33 @@
+package dev.neocamp.swaggerdemo.plant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/plant")
+public class PlantController implements PlantApi {
+    @Autowired
+    private PlantService service;
+
+    @Override
+    @GetMapping(value = "/all", produces = "application/json")
+    public ResponseEntity<List<Plant>> getAll() {
+        var plants = service.getAll();
+        return ResponseEntity.ok(plants);
+    }
+
+    @Override
+    @PostMapping
+    public ResponseEntity<Void> addPlant(Plant plant) {
+        var result = service.addPlant(plant);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
